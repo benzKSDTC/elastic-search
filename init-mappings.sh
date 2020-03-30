19bc7a7275f90c628b1213ca2740d8c749146788
Benz Kritsada <kritsadaksdtc@gmail.com>
2020-03-30 12:45:05 +0700

    feat: docker-compose es0 and es1

--- init-mappings.sh
+++ init-mappings.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 BASENAME=`cat mapping.json`
-
+elasticsearchUrl=$ELASTICSEARCH_URL
 exSuccess=false
 
 for i in {30..0}; do
-    if curl elasticsearch:9200; then
+    if curl $elasticsearchUrl; then
         for row in $(echo "${BASENAME}" | jq -r '.[] | @base64'); do
             _jq() {
             echo ${row} | base64 --decode | jq -r ${1}
             }
-        curl -X PUT "elasticsearch:9200/$(_jq '.index')?pretty" -H 'Content-Type: application/json' -d"$(_jq '.mapping')"
+        curl -X PUT "$elasticsearchUrl/$(_jq '.index')?pretty" -H 'Content-Type: application/json' -d"$(_jq '.mapping')"
         done
         exSuccess=true
         break;
